7fe0fe603ff975c1aed02dfb5a6a375b5801230d
Rasa Hosseinzdaeh <rasa.hosseinzadeh@gmail.com>
2025-06-30 11:00:21 -0400

    Remove space after range colon.

--- src/tabdpt/classifier.py
+++ src/tabdpt/classifier.py
@@ -90,7 +90,7 @@ class TabDPTClassifier(TabDPTEstimator, ClassifierMixin):
             else:
                 pred = self._predict_large_cls(X_train, X_test, y_train)
 
-            pred = pred[..., : self.num_classes] / temperature
+            pred = pred[..., :self.num_classes] / temperature
             pred = torch.nn.functional.softmax(pred.float(), dim=-1)
             pred_val = pred.squeeze().detach().cpu().numpy()
         else:
@@ -121,7 +121,7 @@ class TabDPTClassifier(TabDPTEstimator, ClassifierMixin):
 
                 pred = pred.float()
                 if not return_logits:
-                    pred = pred[..., : self.num_classes] / temperature
+                    pred = pred[..., :self.num_classes] / temperature
                     pred = torch.nn.functional.softmax(pred, dim=-1)
                     pred /= pred.sum(axis=-1, keepdims=True)  # numerical stability
 
@@ -141,7 +141,7 @@ class TabDPTClassifier(TabDPTEstimator, ClassifierMixin):
             else:
                 logits_cumsum += logits
 
-        pred = (logits_cumsum / n_ensembles)[..., : self.num_classes] / temperature
+        pred = (logits_cumsum / n_ensembles)[..., :self.num_classes] / temperature
         pred = softmax(pred, axis=-1)
         pred /= pred.sum(axis=-1, keepdims=True)  # numerical stability
         return pred
